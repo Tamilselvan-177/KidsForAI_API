b1486de5346b59d36825a6be48be37e7f196c0b2
Tamilselvan-177 <231801177@rajalakshmi.edu.in>
2025-08-03 12:43:42 +0530

    requirements.txt ad

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = "mysql+mysqlconnector://root:AkTAMIL7708#@localhost:3306/kids"  # Change this
+# DATABASE_URL = "postgresql+asyncpg://kidsforai_user:@dpg-d27ggtp5pdvs73fjots0-a.oregon-postgres.render.com:5432/kidsforai"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
